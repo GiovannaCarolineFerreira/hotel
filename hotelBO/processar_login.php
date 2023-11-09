afb8456717ab7ba7d5a9e69af392e10f3e22d5db
GiovannaCarolineFerreira <91256057+GiovannaCarolineFerreira@users.noreply.github.com>
2023-11-09 10:10:33 -0300

    Update processar_login.php

--- hotelBO/processar_login.php
+++ hotelBO/processar_login.php
@@ -6,8 +6,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // verificando se o usuário logado é "admin" e a senha é "admin1234"
-    if ($username === "admin" && $password === "admin1234") {
+    // verificando se o usuário logado é "admin" e a senha é "admin123"
+    if ($username === "admin" && $password === "admin123") {
         $_SESSION["username"] = $username;
         header("Location: home.php");
         exit();
